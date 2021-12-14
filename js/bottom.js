d88400abb5ec38a41200819cdfb76a7ff923d1c0
Jose-Banegas <josepb3@gmail.com>
2021-12-14 13:27:10 -0300

    bottom terminado

--- js/bottom.js
+++ js/bottom.js
@@ -5,35 +5,30 @@ const herramientas2 = document.getElementById("herr2");
 const dire1 = document.getElementById("dire1");
 const dire2 = document.getElementById("dire2");
 const direDiv = document.getElementById("dire");
-
+const divPadre = document.getElementById("footer-izq");
+const divH3 = document.getElementById("izq-container");
 const h3 = document.getElementById("h3Dire");
+
+divH3.style.textAlign = "center";
+var posible = true;
+
 
 	if (window.innerWidth < 1024) {
 			footerTop.style.display = "block";
 			footerBot.style.display = "block";
+
+			posible ? (direDiv.appendChild(divH3), posible = false) : null
 	
 		}
 
-	if (window.innerWidth < 1366) {
-			herramientas1.style.display = "none";
-			herramientas2.style.display = "none";
-			dire1.style.display = "none";
-			dire2.style.display = "none";
 
-	}
+
 
 	if (window.innerWidth >= 1024) {
 			footerTop.style.display = "grid";
 			footerBot.style.display = "grid";
-		
 
-	}
 
-	if (window.innerWidth >= 1366) {
-			herramientas1.style.display = "initial";
-			herramientas2.style.display = "initial";
-			dire1.style.display = "initial";
-			dire2.style.display = "initial";
 
 	}
 
@@ -41,32 +36,21 @@ window.addEventListener("resize", function() {
 		if (window.innerWidth < 1024) {
 			footerTop.style.display = "block";
 			footerBot.style.display = "block";
+			posible ? (direDiv.appendChild(divH3), posible = false) : null
 		
 		}
 
-	if (window.innerWidth < 1366) {
-			herramientas1.style.display = "none";
-			herramientas2.style.display = "none";
-			dire1.style.display = "none";
-			dire2.style.display = "none";
 
-	}
 
 	if (window.innerWidth >= 1024) {
 			footerTop.style.display = "grid";
 			footerBot.style.display = "grid";
+			-posible ? (divPadre.appendChild(divH3), posible = true) : null
 
 	
 
 	}
 
-	if (window.innerWidth >= 1366) {
-		herramientas1.style.display = "initial";
-		herramientas2.style.display = "initial";
-		dire1.style.display = "initial";
-		dire2.style.display = "initial";
-
-	}
 	
 
 })
