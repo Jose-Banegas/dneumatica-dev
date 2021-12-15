aab3afb1103274b02224b152082d4f7679db3199
Jose-Banegas <josepb3@gmail.com>
2021-12-15 15:33:29 -0300

    Falta optimizar codigo

--- js/scrollResponsive.js
+++ js/scrollResponsive.js
@@ -2,6 +2,26 @@
 
 const padre = document.getElementById("scrollPadre");
 
+// padres de cada texto e img
+
+const padreBox1 = document.getElementById("padreBox1")
+const padreBox2 = document.getElementById("padreBox2")
+const padreBox3 = document.getElementById("padreBox3")
+const padreBox4 = document.getElementById("padreBox4")
+const padreBox5 = document.getElementById("padreBox5")
+const padreBox6 = document.getElementById("padreBox6")
+
+const padreImg1 = document.getElementById("padreImg1")
+const padreImg2 = document.getElementById("padreImg2")
+const padreImg3 = document.getElementById("padreImg3")
+const padreImg4 = document.getElementById("padreImg4")
+const padreImg5 = document.getElementById("padreImg5")
+const padreImg6 = document.getElementById("padreImg6")
+
+
+
+// texto e img
+
 const texto1 = document.getElementById("box1");
 const texto2 = document.getElementById("box2");
 const texto3 = document.getElementById("box3");
@@ -10,12 +30,13 @@ const texto5 = document.getElementById("box5");
 const texto6 = document.getElementById("box6");
 
 const imagen1 = document.getElementById("imagen1");
-
 const imagen2 = document.getElementById("imagen2");
 const imagen3 = document.getElementById("imagen3");
 const imagen4 = document.getElementById("imagen4");
 const imagen5 = document.getElementById("imagen5");
 const imagen6 = document.getElementById("imagen6");
+
+// nuevas rows
 
 const renglon1 = document.createElement("div");
 const renglon2 = document.createElement("div");
@@ -170,10 +191,33 @@ window.addEventListener("resize", () => {
 	renglon6.style.margin = "auto"
 	imagen6.style.width = "90%";
 	imagen6.style.borderRadius = "15px";
-	texto6.style.justifyContent = "left";
 	renglon6.appendChild(texto6);
 	renglon6.appendChild(imagen6);
 	padre.appendChild(renglon6);
-}
+	}
+
+	if (window.innerWidth >= 1024) {
+		padreBox1.appendChild(texto1)
+		padreBox2.appendChild(texto2)
+		padreBox3.appendChild(texto3)
+		padreBox4.appendChild(texto4)
+		padreBox5.appendChild(texto5)
+		padreBox6.appendChild(texto6)
+
+		padreImg1.appendChild(imagen1)
+		padreImg2.appendChild(imagen2)
+		padreImg3.appendChild(imagen3)
+		padreImg4.appendChild(imagen4)
+		padreImg5.appendChild(imagen5)
+		padreImg6.appendChild(imagen6)
+		padre.removeChild(renglon1)
+		padre.removeChild(renglon2)
+		padre.removeChild(renglon3)
+		padre.removeChild(renglon4)
+		padre.removeChild(renglon5)
+		padre.removeChild(renglon6)
+
+
+	}
 	
 })
